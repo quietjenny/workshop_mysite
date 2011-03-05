5a428bbde469be9703b359e7c83f539c39d964f3
Jennifer Kiehling <jkiehling@gmail.com>
2011-03-05 17:55:57 -0500

    More changes

--- urls.py
+++ urls.py
@@ -1,21 +1,21 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^polls/$', 'polls.views.index'),
     (r'^polls/(\d+)/$', 'polls.views.detail'),
     (r'^polls/(\d+)/results/$', 'polls.views.results'),
     (r'^polls/(\d+)/vote/$', 'polls.views.vote'),
-)
+
     # Example:
     # (r'^mysite/', include('mysite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-
+    (r'^admin/', include(admin.site.urls)),
+)
